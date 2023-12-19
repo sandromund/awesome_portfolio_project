c17cad00adc467cc335070fcef6affff885497ee
Sandro <31536101+sandromund@users.noreply.github.com>
2023-12-19 16:34:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-import time
 
 import customtkinter as ct
-import tkinter as t
 
 
 class App(ct.CTk):
 
     def __init__(self):
         super().__init__()
-        #self.textbox = ct.CTkTextbox(master=self)
-        self.x_label = ct.CTkLabel(self, text="Input X")
-        self.y_label = ct.CTkLabel(self, text="Input Y")
-        self.x = ct.CTkEntry(self, placeholder_text="4")
-        self.y = ct.CTkEntry(self, placeholder_text="20")
-        self.button = ct.CTkButton(master=self, text="Complex Calculation", command=self.button_pressed)
+        self.textbox = ct.CTkTextbox(master=self)
+        self.label = ct.CTkLabel(self, text="X + Y = ? ")
+        self.x = ct.CTkEntry(self, placeholder_text="Enter a number X")
+        self.y = ct.CTkEntry(self, placeholder_text="Enter a number Y")
+        self.button = ct.CTkButton(master=self, text="Addition", command=self.button_pressed)
         self.progressbar = ct.CTkProgressBar(master=self)
         # settings
         self.title("AI Inside")
@@ -22,22 +19,23 @@ class App(ct.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
         # placements
-        args = {"padx": 20, "pady": 10, "sticky": "nsew"}
-        # self.textbox.grid(**args)
-        self.x_label.grid(row=0, column=0, **args)
-        self.x.grid(row=0, column=1, **args)
-        self.y_label.grid(row=1, column=0, **args)
-        self.y.grid(row=1, column=1, **args)
-        self.progressbar.grid(row=2, columnspan=2, **args)
-        self.button.grid(row=3, columnspan=2, **args)
+        args = {"padx": 10, "pady": 5, "sticky": "ew"}
+        self.label.grid(row=0, **args)
+        self.x.grid(row=1, columnspan=2, **args)
+        self.y.grid(row=2, columnspan=2, **args)
+        self.textbox.grid(row=3, columnspan=2, **args)
+        self.progressbar.grid(row=4, columnspan=2, **args)
+        self.button.grid(row=5, columnspan=2, **args)
 
     def button_pressed(self, value=0):
-        x = self.textbox.get(1.0, t.END)
         if not (0 <= value <= 1):
-            self.textbox.insert("0.0", str(x) + "\n")
+            x = self.x.get()
+            y = self.y.get()
+            answer = f"X={x} + Y={y} = {x + y} \n"
+            self.textbox.insert("0.0", answer)
             return
         self.progressbar.set(value)
-        app.after(1500, self.button_pressed, value + 0.2)
+        app.after(500, self.button_pressed, value + 0.2)
 
 
 if __name__ == '__main__':
